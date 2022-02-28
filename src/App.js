6a4bc3293d869ff72279cebdddb6ad5c2d201b51
Eric <eric.morales@protonmail.com>
2022-02-27 21:26:56 -0400

    content component built

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Title from './Title'
+import POPPOSList from './POPOSList';
 
 function App() {
   return (
     <div className='App'>
       <img src={logo} className="App-logo" alt="logo" />
       <Title />
+      <POPPOSList />
     </div>
   );
 }
---
+++ src/POPOSSpace.js
@@ -0,0 +1,13 @@
+function POPOSSpace(props) {
+    const { name, image, address } = props
+    return (
+        <div>
+            <img src={`${process.env.PUBLIC_URL}/images/${image}`} 
+            width='300' height='300' alt={address}/>
+            <h1>{name}</h1>
+            <div>{address}</div>
+        </div>
+    )
+}
+
+export default POPOSSpace
