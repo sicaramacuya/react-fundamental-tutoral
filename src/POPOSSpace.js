5031a54ff36f71270b5a88ee8aa18f17dac30ae2
Eric <eric.morales@protonmail.com>
2022-02-28 14:49:47 -0400

    dynamic content and footer

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -1,13 +1,14 @@
 import './POPOSSpace.css'
 
 function POPOSSpace(props) {
-    const { name, image, address } = props
+    const { name, image, address, hours } = props
     return (
         <div className='POPOSSpace'>
             <img src={`${process.env.PUBLIC_URL}/images/${image}`} 
             width='300' height='300' alt={address}/>
             <h1>{name}</h1>
             <div>{address}</div>
+            <div>{hours}</div>
         </div>
     )
 }
